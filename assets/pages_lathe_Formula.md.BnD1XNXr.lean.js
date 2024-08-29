5e7368d7955e63d238306731e771fc2f99318d3f
zxp <xp_1017@163.com>
2024-08-29 15:22:22 +0800

    同步构建

--- assets/pages_lathe_Formula.md.BnD1XNXr.lean.js
+++ assets/pages_lathe_Formula.md.BnD1XNXr.lean.js
@@ -1,4 +1,29 @@
-import{_ as H,W as V,v as j,y as A,ad as b,o as f,c as d,j as p,t as h,k as g,F as v,E as y,$ as M,ae as C,a as T,af as P,ag as I,e as D,I as L}from"./chunks/framework.Cp4JlySx.js";const q=["id"],S=["innerHTML"],z={key:0,type:"radio",disabled:""},N=["value"],R=["colspan"],U=["onUpdate:modelValue"],E=["value"],$=["readonly","onUpdate:modelValue"],W={key:0},B={colspan:"4"},J={style:{"text-align":"left"}},Q={__name:"FormulaWidget",props:{config:{type:Object,required:!0}},setup(w){const l=w.config;function _(e){const o={};for(let t in e)o[t]=e[t].value;return o}function x(e){return typeof e=="number"?parseFloat(e.toFixed(4)):typeof e=="string"?e.toString().toUpperCase():e}function n(e,o){const t=Object.keys(o).length,r=e.reduce((c,u)=>Object.keys(u).length>c?Object.keys(u).length:c,-1/0);return t===1&&r!==1?r:void 0}const a=V({items:(l==null?void 0:l.items)||[],select:Object.values((l==null?void 0:l.items)||[]).filter(e=>!e.disabled).length>0?Object.keys((l==null?void 0:l.items)||[]).slice(-1)[0]:void 0,info:void 0}),m=(l==null?void 0:l.calc)||(()=>({})),i=(e,o)=>{console.log("updateValue",e,o);const t=_(o);if(typeof e=="boolean"||e){const r=m(e,t);if(r&&typeof r=="object"){console.log("result",r);for(let c in r)for(let u in r[c])t[c][u]=x(r[c][u])}console.log("updateValue after",t,o)}else a.info=m(void 0,t).info};return j([()=>a.select,()=>a.items],()=>{console.log("watch",a.select,a.items),i(a.select,a.items)},{immediate:!0,deep:!0}),A(()=>{console.log("mounted",l.name,{items:b(a.items),select:b(a.select)})}),(e,o)=>(f(),d(v,null,[p("h2",{id:g(l).name.toLowerCase().replaceAll(" ","-")},h(g(l).name),9,q),p("section",{innerHTML:g(l).latexHTML},null,8,S),p("table",null,[(f(!0),d(v,null,y(a.items,t=>(f(),d("tr",null,[p("td",null,[t.disabled?(f(),d("input",z)):M((f(),d("input",{key:1,type:"radio",value:t.name,"onUpdate:modelValue":o[0]||(o[0]=r=>a.select=r)},null,8,N)),[[C,a.select]]),T(" "+h(t.name),1)]),(f(!0),d(v,null,y(t.value,(r,c)=>(f(),d("td",{colspan:n(Object.values(_(a.items)),t.value)},[Array.isArray(t.range)?M((f(),d("select",{key:0,"onUpdate:modelValue":u=>t.value[c]=u},[(f(!0),d(v,null,y(t.range,u=>(f(),d("option",{value:u},h(u),9,E))),256))],8,U)),[[P,t.value[c]]]):M((f(),d("input",{key:1,type:"number",readonly:t.name===a.select,"onUpdate:modelValue":u=>t.value[c]=u},null,8,$)),[[I,t.value[c]]])],8,R))),256)),p("td",null,h(t.unit),1),p("td",null,h(t.comment),1)]))),256)),a.info?(f(),d("tr",W,[p("td",B,[p("pre",J,h(a.info),1)])])):D("",!0)])],64))}},K=H(Q,[["__scopeId","data-v-04731ce7"]]),Z={__name:"Formula",setup(w){function s(n="default",a="默认注释",m=void 0,i="默认单位",e={min:-9999.9999,max:9999.9999},o=!1){(m===void 0||typeof m=="number"||typeof m=="string"||typeof m=="boolean"||Array.isArray(m))&&(m={value_:m});const t={name:n,comment:a,value:m,unit:i,range:e||{min:-9999.9999,max:9999.9999},disabled:o};{let r;Array.isArray(m)?r=m:typeof m=="object"?r=[...Object.values(m).flat()]:r=[m],r.every(c=>typeof c=="boolean")?t.inType="boolean":r.every(c=>typeof c=="string")?t.inType="string":r.every(c=>typeof c=="number")&&(t.inType="number")}return t}function l(...n){const a={};return n.forEach((m,i)=>{a[m.name]=m}),a}const _={Capacity:{name:"功率（Pc）",latexHTML:`
+import{_ as H,W as V,v as j,y as A,ad as b,o as f,c as d,j as p,t as h,k as g,F as v,E as y,$ as M,ae as C,a as T,af as P,ag as I,e as D,I as L}from"./chunks/framework.Cp4JlySx.js";const q=["id"],S=["innerHTML"],z={key:0,type:"radio",disabled:""},N=["value"],R=["colspan"],U=["onUpdate:modelValue"],E=["value"],$=["readonly","onUpdate:modelValue"],W={key:0},B={colspan:"4"},J={style:{"text-align":"left"}},Q={__name:"FormulaWidget",props:{config:{type:Object,required:!0}},setup(w){const l=w.config;function _(e){const o={};for(let t in e)o[t]=e[t].value;return o}function x(e){return typeof e=="number"?parseFloat(e.toFixed(4)):typeof e=="string"?e.toString().toUpperCase():e}function n(e,o){const t=Object.keys(o).length,r=e.reduce((c,u)=>Object.keys(u).length>c?Object.keys(u).length:c,-1/0);return t===1&&r!==1?r:void 0}const a=V({items:(l==null?void 0:l.items)||[],select:Object.values((l==null?void 0:l.items)||[]).filter(e=>!e.disabled).length>0?Object.keys((l==null?void 0:l.items)||[]).slice(-1)[0]:void 0,info:void 0}),m=(l==null?void 0:l.calc)||(()=>({})),i=(e,o)=>{console.log("updateValue",e,o);const t=_(o);if(typeof e=="boolean"||e){const r=m(e,t);if(r&&typeof r=="object"){console.log("result",r);for(let c in r)for(let u in r[c])t[c][u]=x(r[c][u])}console.log("updateValue after",t,o)}else a.info=m(void 0,t).info};return j([()=>a.select,()=>a.items],()=>{console.log("watch",a.select,a.items),i(a.select,a.items)},{immediate:!0,deep:!0}),A(()=>{console.log("mounted",l.name,{items:b(a.items),select:b(a.select)})}),(e,o)=>(f(),d(v,null,[p("h2",{id:g(l).name.toLowerCase().replaceAll(" ","-")},h(g(l).name),9,q),p("section",{innerHTML:g(l).latexHTML},null,8,S),p("table",null,[(f(!0),d(v,null,y(a.items,t=>(f(),d("tr",null,[p("td",null,[t.disabled?(f(),d("input",z)):M((f(),d("input",{key:1,type:"radio",value:t.name,"onUpdate:modelValue":o[0]||(o[0]=r=>a.select=r)},null,8,N)),[[C,a.select]]),T(" "+h(t.name),1)]),(f(!0),d(v,null,y(t.value,(r,c)=>(f(),d("td",{colspan:n(Object.values(_(a.items)),t.value)},[Array.isArray(t.range)?M((f(),d("select",{key:0,"onUpdate:modelValue":u=>t.value[c]=u},[(f(!0),d(v,null,y(t.range,u=>(f(),d("option",{value:u},h(u),9,E))),256))],8,U)),[[P,t.value[c]]]):M((f(),d("input",{key:1,type:"number",readonly:t.name===a.select,"onUpdate:modelValue":u=>t.value[c]=u},null,8,$)),[[I,t.value[c]]])],8,R))),256)),p("td",null,h(t.unit),1),p("td",null,h(t.comment),1)]))),256)),a.info?(f(),d("tr",W,[p("td",B,[p("pre",J,h(a.info),1)])])):D("",!0)])],64))}},K=H(Q,[["__scopeId","data-v-04731ce7"]]),Z={__name:"Formula",setup(w){function s(n="default",a="默认注释",m=void 0,i="默认单位",e={min:-9999.9999,max:9999.9999},o=!1){(m===void 0||typeof m=="number"||typeof m=="string"||typeof m=="boolean"||Array.isArray(m))&&(m={value_:m});const t={name:n,comment:a,value:m,unit:i,range:e||{min:-9999.9999,max:9999.9999},disabled:o};{let r;Array.isArray(m)?r=m:typeof m=="object"?r=[...Object.values(m).flat()]:r=[m],r.every(c=>typeof c=="boolean")?t.inType="boolean":r.every(c=>typeof c=="string")?t.inType="string":r.every(c=>typeof c=="number")&&(t.inType="number")}return t}function l(...n){const a={};return n.forEach((m,i)=>{a[m.name]=m}),a}const _={CuttingSpeed:{name:"切削速度（vc）",latexHTML:`
+      <math xmlns="http://www.w3.org/1998/Math/MathML" style="display:math;font-size: x-large;">
+        <mi>v</mi>
+        <mi>c</mi>
+        <mo>=</mo>
+        <mfrac>
+          <mrow>
+            <mi>π</mi>
+            <mo>×</mo>
+            <mi>D</mi>
+            <mi>m</mi>
+            <mo>×</mo>
+            <mi>n</mi>
+          </mrow>
+          <mn>1000</mn>
+        </mfrac>
+        <mo stretchy="false">(</mo>
+        <mi>m</mi>
+        <mrow>
+          <mo>/</mo>
+        </mrow>
+        <mi>m</mi>
+        <mi>i</mi>
+        <mi>n</mi>
+        <mo stretchy="false">)</mo>
+      </math>`,items:l(s("dm","工件材料直径",1,"mm"),s("n","主轴转速",2,"rev/min"),s("vc","切削速度",3,"m/min")),calc:(n,a)=>{const{dm:{value_:m},n:{value_:i},vc:{value_:e}}=a,o={dm:()=>e*1e3/(Math.PI*i),n:()=>e*1e3/(Math.PI*m),vc:()=>Math.PI*m*i/1e3};if(n)return{[n]:{value_:o[n]()}}}},Capacity:{name:"功率（Pc）",latexHTML:`
       <math xmlns="http://www.w3.org/1998/Math/MathML" style="display:math;font-size: x-large;">
         <mi>P</mi>
         <mi>c</mi>
@@ -68,32 +93,7 @@ import{_ as H,W as V,v as j,y as A,ad as b,o as f,c as d,j as p,t as h,k as g,F 
         <mi>i</mi>
         <mi>n</mi>
         <mo stretchy="false">)</mo>
-      </math>`,items:l(s("fr","每转进给量",1,"mm/rev"),s("n","主轴转速",2,"rev/min"),s("vf","主轴(Z轴)进给速度",3,"mm/min")),calc:(n,a)=>{const{fr:{value_:m},n:{value_:i},vf:{value_:e}}=a,o={fr:()=>e/i,n:()=>e/m,vf:()=>m*i};if(n)return{[n]:{value_:o[n]()}}}},CuttingSpeed:{name:"切削速度（vc）",latexHTML:`
-      <math xmlns="http://www.w3.org/1998/Math/MathML" style="display:math;font-size: x-large;">
-        <mi>v</mi>
-        <mi>c</mi>
-        <mo>=</mo>
-        <mfrac>
-          <mrow>
-            <mi>π</mi>
-            <mo>×</mo>
-            <mi>D</mi>
-            <mi>m</mi>
-            <mo>×</mo>
-            <mi>n</mi>
-          </mrow>
-          <mn>1000</mn>
-        </mfrac>
-        <mo stretchy="false">(</mo>
-        <mi>m</mi>
-        <mrow>
-          <mo>/</mo>
-        </mrow>
-        <mi>m</mi>
-        <mi>i</mi>
-        <mi>n</mi>
-        <mo stretchy="false">)</mo>
-      </math>`,items:l(s("dm","工件材料直径",1,"mm"),s("n","主轴转速",2,"rev/min"),s("vc","切削速度",3,"m/min")),calc:(n,a)=>{const{dm:{value_:m},n:{value_:i},vc:{value_:e}}=a,o={dm:()=>e*1e3/(Math.PI*i),n:()=>e*1e3/(Math.PI*m),vc:()=>Math.PI*m*i/1e3};if(n)return{[n]:{value_:o[n]()}}}},Feedrate:{name:"进给速度（F）",latexHTML:`
+      </math>`,items:l(s("fr","每转进给量",1,"mm/rev"),s("n","主轴转速",2,"rev/min"),s("vf","主轴(Z轴)进给速度",3,"mm/min")),calc:(n,a)=>{const{fr:{value_:m},n:{value_:i},vf:{value_:e}}=a,o={fr:()=>e/i,n:()=>e/m,vf:()=>m*i};if(n)return{[n]:{value_:o[n]()}}}},Feedrate:{name:"进给速度（F）",latexHTML:`
 			  <math xmlns="http://www.w3.org/1998/Math/MathML" style="display:'math';font-size: larger;">
 				<mi>f</mi>
 				<mo>=</mo>
